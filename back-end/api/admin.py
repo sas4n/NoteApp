eb26151a3ffbb5c43602858223f27303b96bfb25
Rahim Pormah <sas4n.porm4h@gmail.com>
2024-03-14 17:28:39 +0100

    Admin registration done

--- back-end/api/admin.py
+++ back-end/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Note
 
 # Register your models here.
+#admin.site.register(Note)
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_filter = ('title', 'description')
+    list_display = ('title', 'description')
