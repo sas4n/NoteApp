7019dc10254b0e0fb3d3d57ffd44abb16760fb82
Rahim Pormah <sas4n.porm4h@gmail.com>
2024-03-14 18:49:28 +0100

    serializer added

---
+++ back-end/api/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Note
+
+class NoteSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=120)
+    description = serializers.CharField(max_length=400)
+
+
+    def create(self, validated_data):
+        return Note.objects.create(validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)('title', instance.title)
+
